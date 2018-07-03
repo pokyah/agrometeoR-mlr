6b171069624d1fd1537b8d39ff93739458898547
pokyah <thomas.goossens@protonmail.com>
2018-07-03 14:14:26 +0000

    cleaning prepare mlr benchamrk

--- ld_devs/explorations/mlr_preparation/mlr_preparation.R
+++ ld_devs/explorations/mlr_preparation/mlr_preparation.R
@@ -15,11 +15,12 @@ source("./R/file_management.R")
 source_files_recursively.fun("./R")
 source_files_recursively.fun("./ld_devs/explorations/mlr_preparation/R")
 
-# install.packages('FSelector')
+#install.packages('FSelector')
 library(FSelector)
 library(tidyverse)
 library(sf)
 library(mlr)
+
 
 #+ ---------------------------------
 #' ## Data acquisition
@@ -29,21 +30,13 @@ library(mlr)
 #' ### Dependent variables
 
 # static variables for stations
-load("./data/expl.static.stations.df.rda")
 
-#load("./data/expl.static.stations.sf.rda")
-
-# libraries
-library(mlr)
-library(tidyr)
-library(dplyr)
-
+# load("./data/expl.static.stations.sf.rda")
 load("./data/expl.static.stations.sf.rda")
-
 
 #### 1 day ####
 # Retrieving from API
-dynamic.records.1day.df <- prepare_agromet_API_data.fun(
+records.stations.df <- prepare_agromet_API_data.fun(
   get_from_agromet_API.fun(
     user_token.chr = Sys.getenv("AGROMET_API_V1_KEY"),
     table_name.chr = "cleandata",
@@ -55,8 +48,8 @@ dynamic.records.1day.df <- prepare_agromet_API_data.fun(
   ), "cleandata"
 )
 
-# Filtering records to keep only the useful ones
-dynamic.records.1day.df <- dynamic.records.1day.df %>%
+# Filtering dynamic records to keep only the useful ones
+records.stations.df <- records.stations.df %>%
   filter(network_name == "pameseb") %>%
   filter(type_name != "Sencrop") %>%
   filter(!is.na(to)) %>%
@@ -64,62 +57,61 @@ dynamic.records.1day.df <- dynamic.records.1day.df %>%
   filter(!is.na(tsa)) %>%
   filter(!is.na(ens))
 
-# Filtering records to keep only the useful ones
+# Filtering static records to keep only the useful ones
 expl.static.stations.sf <- expl.static.stations.sf %>%
   filter(gid != 36 & gid != 41)
 
 # Selecting only the useful features
-dynamic.records.1day.df <- dynamic.records.1day.df %>%
+records.stations.df <- records.stations.df %>%
   dplyr::select("mtime", "sid", "tsa" ,"ens", "longitude", "latitude")
-colnames(dynamic.records.1day.df)[2] <- "gid"
+colnames(records.stations.df)[2] <- "gid"
 
-dynamic.records.1day.sf <- st_as_sf(dynamic.records.1day.df, coords = c("longitude", "latitude"))
-st_crs(dynamic.records.1day.sf) <- "+proj=longlat +datum=WGS84 +no_defs"
+# Preparing for spatial join of dynamic and static expl vars
+records.stations.sf <- st_as_sf(records.stations.df, coords = c("longitude", "latitude"))
+st_crs(records.stations.sf) <- "+proj=longlat +datum=WGS84 +no_defs"
 lambert2008.crs <- "+proj=lcc +lat_1=49.83333333333334 +lat_2=51.16666666666666 +lat_0=50.797815 +lon_0=4.359215833333333 +x_0=649328 +y_0=665262 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-dynamic.records.1day.sf <- st_transform(dynamic.records.1day.sf, crs = lambert2008.crs)
+records.stations.sf <- st_transform(records.stations.sf, crs = lambert2008.crs)
+data.sf <- st_join(expl.static.stations.sf, records.stations.sf)
 
-
+# Make the sf objects, df again
+data.stations.df <- data.frame(data.sf) %>%
+  dplyr::select(-geometry) %>%
+  dplyr::select(-gid.y) %>%
+  dplyr::select(-gid.x)
 
 # Building a nested data frame, where for each hourly observation we have a 27 stations dataset of 1h record.
-expl.stations.df <- left_join(expl.static.stations.df, dynamic.records.df, by = "gid")
-
-# Reordering columns for better readability
-expl.stations.nested.df <- expl.stations.df[, c(13,1,2,3,4,5,6,7,8,9,14,15,11,12,10)]
-
-# Cleaning to only keep the explanatory vars and nesting for batch benchmark
-expl.stations.nested.df %>%
-
-# Building a nested data frame, where for each hourly observation we have a 30 stations dataset of 1h record.
-expl.stations.1day.sf <- st_join(expl.static.stations.sf, dynamic.records.1day.sf)
-expl.stations.1day.df <- data.frame(expl.stations.1day.sf) %>%
-  select(-geometry) %>%
-  select(-gid.y)
-names(expl.stations.1day.df)[1] <- "gid"
-expl.stations.nested.1day.df <- expl.stations.1day.df[, c(10,1,2,3,4,5,6,7,8,9,11,12)] %>%
+data.stations.n.df <- data.stations.df %>%
   group_by(mtime) %>%
   tidyr::nest() %>%
   filter(mtime <= "2018-06-11 23:00:00" & mtime >= "2018-06-11 00:00:00")
 
+# converting each tibble of the nested records to a strict dataframe (required by mlr)
+# ::todo:: need to use transmute_at
+data.stations.n.df <- data.stations.n.df %>%
+  mutate(data_as_df = purrr::map(
+    .x = data,
+    .f = data.frame
+  ))
+
+# removing the tibbles columns and only keeping the pure dataframes (required by mlr)
+data.stations.n.df <- data.stations.n.df %>%
+  dplyr::select(mtime, data_as_df)
 
 #+ ---------------------------------
 #' ## mlr benchmark experiment preparation
 #'
 #+ data-acquisition, echo=TRUE, warning=FALSE, message=FALSE, error=FALSE, results='asis'
 
-
-# bmr.l <- benchmark.hourly_sets(nested.records.df = expl.stations.nested.df, target.chr = "tsa")
-
-
 # defining the target var
 target.chr = "tsa"
 
 # defining the learners who will be compared
 lrns.l <- list(
-  makeFilterWrapper(
-    learner = makeLearner(cl = "regr.lm", id="linear regression"),  fw.method = "information.gain", fw.abs = 2),
-  # makeLearner(cl = "regr.lm", id="linear regression"),
-  # makeLearner(cl = "regr.elmNN", id="single layer neural net"),
-  makeLearner(cl ="regr.kknn", id="nearest neighbours")#,
+  # makeFilterWrapper(
+  # learner = makeLearner(cl = "regr.lm", id="linear regression"),  fw.method = "information.gain", fw.abs = 2),
+  makeLearner(cl = "regr.lm", id="linear regression"),
+  makeLearner(cl = "regr.elmNN", id="single layer neural net"),
+  makeLearner(cl ="regr.kknn", id="nearest neighbours")
   # makeLearner(cl = "regr.km", id="kriging")
 )
 
@@ -129,32 +121,17 @@ resampling.l = mlr::makeResampleDesc(
   #predict = "test"
 )
 
-# converting each tibble of the nested records to a strict dataframe (required by mlr)
-# ::todo:: need to use transmute_at
-expl.stations.nested.1day.df <- expl.stations.nested.1day.df %>%
-  mutate(data_as_df = purrr::map(
-    .x = data,
-    .f = data.frame
-  ))
-
 # defining the regression tasks on the stations observations for each of the hourly datasets
 # https://stackoverflow.com/questions/46868706/failed-to-use-map2-with-mutate-with-purrr-and-dplyr
 # https://stackoverflow.com/questions/42518156/use-purrrmap-to-apply-multiple-arguments-to-a-function?rq=1
-expl.stations.nested.1day.df <- expl.stations.nested.1day.df %>%
-  mutate(all_vars.stations.task = purrr::map2(
+data.stations.n.df <- data.stations.n.df %>%
+  mutate(tasks = purrr::map2(
     as.character(mtime),
     data_as_df,
     mlr::makeRegrTask,
     target = target.chr
   )
   )
-
-expl.stations.nested.1day.df <- expl.stations.nested.1day.df %>%
-  mutate(all_vars.stations.task2 = purrr::map2(
-    all_vars.stations.task,
-    "gid",
-    mlr::dropFeatures
-  ))
 
 # fusing various tasks to the learners in the most convenient way
 # https://mlr-org.github.io/mlr/articles/tutorial/devel/preproc.html
@@ -170,289 +147,19 @@ expl.stations.nested.1day.df <- expl.stations.nested.1day.df %>%
 # we also have the option to use an automatic feature selector by fusing it to the learner (see mlr doc).
 # defining the learner who will be used by taking the one with the lowest RMSE from the bmr experiment
 
-bmr.l.1day <- benchmark(
+bmr.l <- benchmark(
   learners = lrns.l,
-  tasks = expl.stations.nested.1day.df$all_vars.stations.task2,
+  tasks = data.stations.n.df$tasks,
   resamplings = resampling.l,
-  keep.pred = TRUE,
+  keep.pred = FALSE,
   show.info = TRUE,
   models = FALSE,
   measures = list(mse, timetrain)
 )
 
-# timetrain = 0.0445556
-
-#### 7 days ####
-# Retrieving from API
-dynamic.records.7days.df <- prepare_agromet_API_data.fun(
-  get_from_agromet_API.fun(
-    user_token.chr = Sys.getenv("AGROMET_API_V1_KEY"),
-    table_name.chr = "cleandata",
-    stations_ids.chr = "all",
-    sensors.chr = "tsa,ens",
-    dfrom.chr = "2018-05-11",
-    dto.chr = "2018-05-18",
-    api_v.chr = "v2"
-  ), "cleandata"
-)
-
-# Filtering records to keep only the useful ones
-dynamic.records.7days.df <- dynamic.records.7days.df %>%
-  filter(network_name == "pameseb") %>%
-  filter(type_name != "Sencrop") %>%
-  filter(!is.na(to)) %>%
-  filter(state == "Ok") %>%
-  filter(!is.na(tsa)) %>%
-  filter(!is.na(ens))
-
-# Filtering records to keep only the useful ones
-expl.static.stations.sf <- expl.static.stations.sf %>%
-  filter(gid != 36 & gid != 41)
-
-# Selecting only the useful features
-dynamic.records.7days.df <- dynamic.records.7days.df %>%
-  dplyr::select("mtime", "sid", "tsa" ,"ens", "longitude", "latitude")
-colnames(dynamic.records.7days.df)[2] <- "gid"
-
-dynamic.records.7days.sf <- st_as_sf(dynamic.records.7days.df, coords = c("longitude", "latitude"))
-st_crs(dynamic.records.7days.sf) <- "+proj=longlat +datum=WGS84 +no_defs"
-lambert2008.crs <- "+proj=lcc +lat_1=49.83333333333334 +lat_2=51.16666666666666 +lat_0=50.797815 +lon_0=4.359215833333333 +x_0=649328 +y_0=665262 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-dynamic.records.7days.sf <- st_transform(dynamic.records.7days.sf, crs = lambert2008.crs)
-
-
-# Building a nested data frame, where for each hourly observation we have a 30 stations dataset of 1h record.
-expl.stations.7days.sf <- st_join(expl.static.stations.sf, dynamic.records.7days.sf)
-expl.stations.7days.df <- data.frame(expl.stations.7days.sf) %>%
-  select(-geometry) %>%
-  select(-gid.y)
-names(expl.stations.7days.df)[1] <- "gid"
-expl.stations.nested.7days.df <- expl.stations.7days.df[, c(10,1,2,3,4,5,6,7,8,9,11,12)] %>%
-  group_by(mtime) %>%
-  nest() %>%
-  filter(mtime <= "2018-05-17 23:00:00" & mtime >= "2018-05-11 00:00:00")
-
-
-#+ ---------------------------------
-#' ## mlr benchmark experiment preparation
-#'
-#+ data-acquisition, echo=TRUE, warning=FALSE, message=FALSE, error=FALSE, results='asis'
-
-
-# bmr.l <- benchmark.hourly_sets(nested.records.df = expl.stations.nested.df, target.chr = "tsa")
-
-
-# defining the target var
-target.chr = "tsa"
-
-# defining the learners who will be compared
-lrns.l <- list(
-  makeFilterWrapper(
-    learner = makeLearner(cl = "regr.lm", id="linear regression"),  fw.method = "information.gain", fw.abs = 2)#,
-  # makeLearner(cl = "regr.lm", id="linear regression"),
-  # makeLearner(cl = "regr.elmNN", id="single layer neural net"),
-  # makeLearner(cl ="regr.kknn", id="nearest neighbours"),
-  # makeLearner(cl = "regr.km", id="kriging")
-)
-
-# defining the validation (resampling) strategy
-resampling.l = mlr::makeResampleDesc(
-  method = "LOO"#,
-  #predict = "test"
-)
-
-# converting each tibble of the nested records to a strict dataframe (required by mlr)
-# ::todo:: need to use transmute_at
-expl.stations.nested.7days.df <- expl.stations.nested.7days.df %>%
-  mutate(data_as_df = purrr::map(
-    .x = data,
-    .f = data.frame
-  ))
-
-# defining the regression tasks on the stations observations for each of the hourly datasets
-# https://stackoverflow.com/questions/46868706/failed-to-use-map2-with-mutate-with-purrr-and-dplyr
-# https://stackoverflow.com/questions/42518156/use-purrrmap-to-apply-multiple-arguments-to-a-function?rq=1
-expl.stations.nested.7days.df <- expl.stations.nested.7days.df %>%
-  mutate(all_vars.stations.task = purrr::map2(
-    as.character(mtime),
-    data_as_df,
-    mlr::makeRegrTask,
-    target = target.chr
-  )
-  )
-
-expl.stations.nested.7days.df <- expl.stations.nested.7days.df %>%
-  mutate(all_vars.stations.task2 = purrr::map2(
-    all_vars.stations.task,
-    "gid",
-    mlr::dropFeatures
-  ))
-
-# fusing various tasks to the learners in the most convenient way
-# https://mlr-org.github.io/mlr/articles/tutorial/devel/preproc.html
-
-#+ ---------------------------------
-#' ## mlr benchmarking
-#'
-#+ benchmarking, echo=TRUE, warning=FALSE, message=FALSE, error=FALSE, results='asis'
-
-# Now we can make a benchmark experiment as described in mlr package.
-# This will allow us to choose the best learner for a specific task.
-# Later, we will need to also define the best task among many tasks (tasks differ by the features that are incorporated)
-# we also have the option to use an automatic feature selector by fusing it to the learner (see mlr doc).
-# defining the learner who will be used by taking the one with the lowest RMSE from the bmr experiment
-
-bmr.l.7days <- benchmark(
-  learners = lrns.l,
-  tasks = expl.stations.nested.7days.df$all_vars.stations.task2,
-  resamplings = resampling.l,
-  keep.pred = TRUE,
-  show.info = TRUE,
-  models = FALSE,
-  measures = list(mse, timetrain)
-)
-
-# timetrain = 0.0468148
-
-#### 30 days ####
-# Retrieving from API
-dynamic.records.30days.df <- prepare_agromet_API_data.fun(
-  get_from_agromet_API.fun(
-    user_token.chr = Sys.getenv("AGROMET_API_V1_KEY"),
-    table_name.chr = "cleandata",
-    stations_ids.chr = "all",
-    sensors.chr = "tsa,ens",
-    dfrom.chr = "2018-05-01",
-    dto.chr = "2018-05-30",
-    api_v.chr = "v2"
-  ), "cleandata"
-)
-
-# Filtering records to keep only the useful ones
-dynamic.records.30days.df <- dynamic.records.30days.df %>%
-  filter(network_name == "pameseb") %>%
-  filter(type_name != "Sencrop") %>%
-  filter(!is.na(to)) %>%
-  filter(state == "Ok") %>%
-  filter(!is.na(tsa)) %>%
-  filter(!is.na(ens))
-
-# Filtering records to keep only the useful ones
-expl.static.stations.sf <- expl.static.stations.sf %>%
-  filter(gid != 36 & gid != 41)
-
-# Selecting only the useful features
-dynamic.records.30days.df <- dynamic.records.30days.df %>%
-  dplyr::select("mtime", "sid", "tsa" ,"ens", "longitude", "latitude")
-colnames(dynamic.records.30days.df)[2] <- "gid"
-
-dynamic.records.30days.sf <- st_as_sf(dynamic.records.30days.df, coords = c("longitude", "latitude"))
-st_crs(dynamic.records.30days.sf) <- "+proj=longlat +datum=WGS84 +no_defs"
-lambert2008.crs <- "+proj=lcc +lat_1=49.83333333333334 +lat_2=51.16666666666666 +lat_0=50.797815 +lon_0=4.359215833333333 +x_0=649328 +y_0=665262 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-dynamic.records.30days.sf <- st_transform(dynamic.records.30days.sf, crs = lambert2008.crs)
-
-
-# Building a nested data frame, where for each hourly observation we have a 30 stations dataset of 1h record.
-expl.stations.30days.sf <- st_join(expl.static.stations.sf, dynamic.records.30days.sf)
-expl.stations.30days.df <- data.frame(expl.stations.30days.sf) %>%
-  select(-geometry) %>%
-  select(-gid.y)
-names(expl.stations.30days.df)[1] <- "gid"
-expl.stations.nested.30days.df <- expl.stations.30days.df[, c(10,1,2,3,4,5,6,7,8,9,11,12)] %>%
-  group_by(mtime) %>%
-  nest() %>%
-  filter(mtime <= "2018-05-30 23:00:00" & mtime >= "2018-05-01 00:00:00")
-
-
-#+ ---------------------------------
-#' ## mlr benchmark experiment preparation
-#'
-#+ data-acquisition, echo=TRUE, warning=FALSE, message=FALSE, error=FALSE, results='asis'
-
-
-# bmr.l <- benchmark.hourly_sets(nested.records.df = expl.stations.nested.df, target.chr = "tsa")
-
-
-# defining the target var
-target.chr = "tsa"
-
-# defining the learners who will be compared
-lrns.l <- list(
-  makeFilterWrapper(
-    learner = makeLearner(cl = "regr.lm", id="linear regression"),  fw.method = "information.gain", fw.abs = 2)#,
-  # makeLearner(cl = "regr.lm", id="linear regression"),
-  # makeLearner(cl = "regr.elmNN", id="single layer neural net"),
-  # makeLearner(cl ="regr.kknn", id="nearest neighbours"),
-  # makeLearner(cl = "regr.km", id="kriging")
-)
-
-# defining the validation (resampling) strategy
-resampling.l = mlr::makeResampleDesc(
-  method = "LOO"#,
-  #predict = "test"
-)
-
-# converting each tibble of the nested records to a strict dataframe (required by mlr)
-# ::todo:: need to use transmute_at
-expl.stations.nested.30days.df <- expl.stations.nested.30days.df %>%
-  mutate(data_as_df = purrr::map(
-    .x = data,
-    .f = data.frame
-  ))
-
-# defining the regression tasks on the stations observations for each of the hourly datasets
-# https://stackoverflow.com/questions/46868706/failed-to-use-map2-with-mutate-with-purrr-and-dplyr
-# https://stackoverflow.com/questions/42518156/use-purrrmap-to-apply-multiple-arguments-to-a-function?rq=1
-expl.stations.nested.30days.df <- expl.stations.nested.30days.df %>%
-  mutate(all_vars.stations.task = purrr::map2(
-    as.character(mtime),
-    data_as_df,
-    mlr::makeRegrTask,
-    target = target.chr
-  )
-  )
-
-expl.stations.nested.30days.df <- expl.stations.nested.30days.df %>%
-  mutate(all_vars.stations.task2 = purrr::map2(
-    all_vars.stations.task,
-    "gid",
-    mlr::dropFeatures
-  ))
-
-# fusing various tasks to the learners in the most convenient way
-# https://mlr-org.github.io/mlr/articles/tutorial/devel/preproc.html
-
-#+ ---------------------------------
-#' ## mlr benchmarking
-#'
-#+ benchmarking, echo=TRUE, warning=FALSE, message=FALSE, error=FALSE, results='asis'
-
-# Now we can make a benchmark experiment as described in mlr package.
-# This will allow us to choose the best learner for a specific task.
-# Later, we will need to also define the best task among many tasks (tasks differ by the features that are incorporated)
-# we also have the option to use an automatic feature selector by fusing it to the learner (see mlr doc).
-# defining the learner who will be used by taking the one with the lowest RMSE from the bmr experiment
-
-bmr.l.30days <- benchmark(
-  learners = lrns.l,
-  tasks = expl.stations.nested.30days.df$all_vars.stations.task2,
-  resamplings = resampling.l,
-  keep.pred = TRUE,
-  show.info = TRUE,
-  models = FALSE,
-  measures = list(mse, timetrain)
-)
-
-# timetrain = 0.0404444
-
-
-# time for 5 years : 24 * (365*4 + 366) * 0.04 /   3600      = 13h17min
-#                  hours   nb_days    timetrain conv in hours
-##
-
-plotBMRBoxplots(bmr.l.1day, measure = mse, style = "violin")
-plotBMRSummary(bmr.l.rlm_kknn.7days)
-plotBMRRanksAsBarChart(bmr.l.7days, pos = "dodge", order.lrn = getBMRLearnerIds(bmr.l.7days))
-plotBMRRanksAsBarChart(bmr.l.rlm_kknn.30days, pos = "dodge", order.lrn = getBMRLearnerIds(bmr.l.rlm_kknn.30days))
+plotBMRBoxplots(bmr.l, measure = mse, style = "violin")
+plotBMRSummary(bmr.l)
+plotBMRRanksAsBarChart(bmr.l, pos = "dodge", order.lrn = getBMRLearnerIds(bmr.l))
 
 # qplot(mmce, colour = learner.id, facets = . ~ task.id,
 #       data = perfs.methods.30days.df[perfs.methods.30days.df$task.id %in% c("2018-05-11 12:00:00", "2018-05-11 13:00:00"),], geom = "density") +
